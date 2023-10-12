584f85d87fcdc0dd6cff4d273f59bb76eba10bdf
freddiebell88 <frederikcwbell@gmail.com>
2023-10-12 19:22:55 -0400

    css grid layout complete made 789 clickable

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log('connected')
+
+const sevenButton = document.querySelector('.seven')
+
+sevenButton.addEventListener('click', () => {
+    console.log("clicked!")
+}) 
+
+const eightButton = document.querySelector('.eight')
+
+eightButton.addEventListener('click', () => {
+    console.log("clicked!")
+})
+
+const nineButton = document.querySelector('.nine')
+
+nineButton.addEventListener('click', () => {
+    console.log("clicked!")
+})
+
+
+// for (let button of buttons) {
+// buttons.addEventListener('click', () => {
+//     console.log("clicked!")
+// })
+// }
